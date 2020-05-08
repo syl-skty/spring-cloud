aa3e0aaecc30e77c4735f938ed563fbe33cb377b
skty <2239120808@qq.com>
2020-05-08 22:06:45 +0800

    Initial commit

---
+++ client/src/test/java/com/skty/springcloud/client/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skty.springcloud.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ server2/src/test/java/com/skty/springcloud/server2/Server2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skty.springcloud.server2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Server2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
