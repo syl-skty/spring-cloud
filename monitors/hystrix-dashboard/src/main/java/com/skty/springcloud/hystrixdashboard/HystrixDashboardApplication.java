66f4cd469641aa28691ce2d8711459cf0fca2362
skty <2239120808@qq.com>
2020-05-12 22:15:45 +0800

    修改host文件

--- monitors/hystrix-dashboard/src/main/java/com/skty/springcloud/hystrixdashboard/HystrixDashboardApplication.java
+++ monitors/hystrix-dashboard/src/main/java/com/skty/springcloud/hystrixdashboard/HystrixDashboardApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
+
+/**
+ * 监控hystrix的工具hystrixDashboard,访问http://localhost:8080/hystrix
+ */
 @EnableHystrixDashboard
 @SpringBootApplication
 public class HystrixDashboardApplication {
