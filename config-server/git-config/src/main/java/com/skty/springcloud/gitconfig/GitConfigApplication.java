e77c7e554778b13c86cee1caab59733d71bae5e0
shiyonglong <yonglongshi@irissz.com>
2020-05-13 15:04:30 +0800

    增加配置文件服务

---
+++ config-server/git-config/src/main/java/com/skty/springcloud/gitconfig/GitConfigApplication.java
@@ -0,0 +1,13 @@
+package com.skty.springcloud.gitconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitConfigApplication.class, args);
+    }
+
+}
---
+++ config-server/git-config/src/test/java/com/skty/springcloud/gitconfig/GitConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skty.springcloud.gitconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
